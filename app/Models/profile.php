9126d0c2e2bf7cb58b075c804c05aeb5e17e9c40
mgalal8866 <58759061+mgalal8866@users.noreply.github.com>
2021-09-11 22:51:30 +0200

    m

--- app/Models/profile.php
+++ app/Models/profile.php
@@ -8,9 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class profile extends Model
 {
   protected $fillable = [
-        'name',
-        'email',
-        'password',
         'province',
         'user_id',
         'gender',
